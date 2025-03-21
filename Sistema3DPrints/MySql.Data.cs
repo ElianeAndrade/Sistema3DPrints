5e83d9bcaf4e1ff8fafa5e81a95d3ac88a973404
Eliane Maria de Andrade <eandrade@tecnologiaunica.com.br>
2025-03-21 15:03:55 -0300

    Start

---
+++ Sistema3DPrints/MySql.Data.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+using System;
+
+public class BancoDeDados
+{
+    private string conexaoString = "Server=srv461.hstgr.io;Database=u802695155_3d;Uid=u802695155_root;Pwd=.Donibus459);";
+
+    public MySqlConnection Conectar()
+    {
+        MySqlConnection conexao = new MySqlConnection(conexaoString);
+        try
+        {
+            conexao.Open();
+            Console.WriteLine("Conectado com sucesso!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao conectar: " + ex.Message);
+        }
+        return conexao;
+    }
+}
